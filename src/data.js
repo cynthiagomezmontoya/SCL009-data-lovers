c416a3b64437791c1c16302166cfffc98f7bcc8c
cynthiagomezmontoya <cgomezm066@gmail.com>
2019-05-16 11:46:54 -0400

    subo cambios

--- src/data.js
+++ src/data.js
@@ -1,56 +1,12 @@
-const pokeData = window.POKEMON.pokemon;
-//Se declara la constante pokeData para almacenar el arreglo de pokemons
-
-
-window.pokeData = pokeData;
-    
-const containerRoot = document.getElementById('root');
-// Se declaran la constante containerRoot para insertar pokemons dentro de este elemento.
-const selectType = document.getElementById('type');
-// Se declaran la constante selectType para insertar type dentro de este elemento
-
-    
- const filterPoketype= (pokeData, type) => {
-//// Se declara la función para filtrar pokemons con los parametros data y condition.
-//El parámetro data está destinado a recibir un arreglo de pokemons.
-//Y el parámetro condition recibe un diccionario con las condiciones por las que se debe filtrar.
-// quemebusque todos los elementos po tipo
-  const resultFiler = pokeData.filter(element => {
-    //lo que que lleva dentro el filter es una funcion
-    // filter, es parecido al for each,pasa por todos los elementos de la data
-    return element.type.includes(type);
-    //includes, pregunta si esta dentro de la data o no ( true o false)
-  })
-
-  return resultFiler;
-};
-
-window.filterPoketype = filterPoketype;
-window.containerRoot= containerRoot;
-window.selectType= selectType;
-
-//ordenar
-
-
-
- sortPokes = (pokeData,sortBy,sortOrder)=> {
-   // El primer parámetro, data, nos entrega los datos.
-   //sortBy, nos dice con respecto a cuál de los campos de la data se quiere ordenar.
-   // sortOrder, indica si se quiere ordenar de manera ascendente o descendente.
-  let ordenaPokes = pokeData;
-  if (sortOrder == "a-z"){
- ordenaPokes.sort((a,b)=> {
-  if (a[sortBy] < b[sortBy]) {return -1;}
-  if (a[sortBy] > b[sortBy]) {return  1;} 
-  return 0;
- })
+const filterLol = (lolData,tags) => { 
+  let result = lolData.filter(element => {
+    return element.tags.includes(tags); 
+  }) 
+  return result;
 }
-if (sortOrder == "z-a"){
-   ordenaPokes.sort((a,b)=> {
-    if (a[sortBy] > b[sortBy]) {return -1;}
-    if (a[sortBy] < b[sortBy]) {return  1;} 
-    return 0;
-   })
- }
- return ordenaPokes;
-}
+
+window.filterLol = filterLol;
+
+
+  
+  
--- src/main.js
+++ src/main.js
@@ -1,45 +1,23 @@
-// filtro DOM
-selectType:"";
-// evento 
-selectType.addEventListener('change', () => {
-   let tipoFilter = selectType.options[selectType.selectedIndex].text;
-   //se crea una variable , al apretar el usuario el select  yo guardar en una variable el valor del select
-   // selectedIndex  indice que quiero sacar 
-   //.text valor del index
-   console.log(tipoFilter);
-  
-   let filtered = filterPoketype(pokeData,tipoFilter);
-    containerRoot.innerHTML = '';
-    // para mostrar la data
-    filtered.forEach(element => {
-     containerRoot.innerHTML += `
-     <div class="img">
-     <img src="${element.img}">
-     </div>
-     <h3>${element.type}</h3>
-     <p>${element.name}</p>`
-    })
-   
-})
+const lolData = Object.values(window.LOL.data); //const para acceder a la data
 
-   
-// ORDENAR
+const container = document.getElementById("root");
+const num = document.getElementById('calculation'); 
 
-const pokeOrder = document.getElementById("name");
-const container = document.getElementById('root');
-
-pokeOrder.addEventListener('change', () => {
-    let sortOrder = pokeOrder.value;
-    let selectionOrder = sortPokes(pokeData,"name",sortOrder);
+ //DOM FUNCIÓN FILTRAR
+ document.getElementById("lol-filter").addEventListener("change",() => {
+    let rolFilter = document.getElementById("lol-filter").value; 
+    let lastResult = window.filterLol(lolData,rolFilter); 
     container.innerHTML ="";
-
-    selectionOrder.forEach(element=> {
+    lastResult.forEach(element => {
         container.innerHTML += `
         <div class="img">
      <img src="${element.img}">
      </div>
-     <h3>${element.name}</h3>
-     <p>${element.type}</p>`
+     <h3>${element.tags}</h3>
+     <p>${element.name}</p>`
+
+     
     })
-   })
- 
+    });
+      
+  
